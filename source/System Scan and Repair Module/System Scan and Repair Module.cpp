ad9c3c0358f36f2ec2e26f3b5af9cec22fc197e2
ANONYMOUSEJR <64444223+ANONYMOUSEJR@users.noreply.github.com>
2023-03-20 23:19:20 +0100

    - Just updated the TODO list with another idea...

--- source/System Scan and Repair Module/System Scan and Repair Module.cpp
+++ source/System Scan and Repair Module/System Scan and Repair Module.cpp
@@ -9,6 +9,7 @@ int main()
 // TODO:
 // - Work on the fullscreen functions.
 // 
+// ~ Create a char counter, with a menu and the ability to allow the user to choose which chars not to include, such as spaces etc...
 // ~ I think that I can make like a puzzle game using the file output stuff, that could be a fun project!
 {
     // To make the program full screen.
