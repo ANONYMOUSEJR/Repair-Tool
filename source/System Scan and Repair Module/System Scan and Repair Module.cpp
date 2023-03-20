29681c2cf0e9d86f57c70c93bb556a4828944d78
ANONYMOUSEJR <64444223+ANONYMOUSEJR@users.noreply.github.com>
2023-03-20 14:05:25 +0100

    CHANGE LOG: - Removed useless goto in loadConf func. - Made TODO list more pretty and updated it. - Created _remFile function to delete corrupted config file. - Implemented a way to check if config values are valid (int check only for now). - Beautified the config file somewhat...
    BONUS:
    - Added an cool idea for another type of game...

--- source/System Scan and Repair Module/System Scan and Repair Module.cpp
+++ source/System Scan and Repair Module/System Scan and Repair Module.cpp
@@ -6,10 +6,14 @@
 using namespace std;
 
 int main()
-/* 
-TODO:
-- Add a func that asks the user if they want to update config file? (???maybe???)
-*/
+// TODO:
+// - Add a func that asks the user if they want to update config file? (???maybe???)
+// - Make a func specifically for checking the variables instead of making spaggheti code??? (nah...)
+//  - Make the function also be able to tell if the given value is a number only... (maybe check if the input between '=' signs is a num)
+// - Prettify the config file documentation. (Mostly done)
+// 
+// ~ I think that i can make like a puzzle game using the file output stuff... that could be a fun project...
+
 {
     // To make the program full screen.
     /*
--- source/System Scan and Repair Module/confile.h
+++ source/System Scan and Repair Module/confile.h
@@ -21,6 +21,16 @@ void _pause(short x) {
 	}
 }
 
+void _remFile() {
+// Attempt to delete the file
+	if (remove("config.txt") != 0) {
+		cerr << "Error deleting file\n"; _pause(3);
+	}
+	else {
+		cout << "File successfully deleted\n"; _pause(3);
+	}
+}
+
 void saveConf(short spd = 1, short power = 0, short colour = 4) {
 	fstream file;
 	file.open("config.txt", ios::out);
@@ -29,11 +39,12 @@ void saveConf(short spd = 1, short power = 0, short colour = 4) {
 		file << setw(20) << "Power settings = " << power << endl;
 		file << setw(20) << "Colour settings = " << colour << endl;
 		// For user info: 
-		file << "# Putting in wrong values might cause issues in the system,\nplease observe the following info to familiarise yourself with the formatting\n\n";
-		file << "1) To allow/disallow the Scrolling effect write a 1/0 next to \"Character scrolling effect\".\n";
-		file << "2) To change Power setting replace the value with one of the following numbers:\n - Shutdown = 1\n - Restart = 2\n - Logout = 3\n - Do Nothing = 0\n";
-		file << "3) To change Colour settings replace the value with one of the following numbers:\n - Light Red = 1\n - Light Purple = 2\n - Light Yellow = 3\n - Light Green = 4\n - Bright White = 5\n";
-		file << "General advice, please do not remove the \"#\" it will cause issues in the program because it is the divider between the stored variables and documentation.\nPlease only enter whole numbers as values to be loaded to avoid issues.\nDon't worry about the distance between the \"=\" sign and the actual variable I just made it like that to make it look pretty lol.";
+		file << "######################";
+		file << "\nPutting in wrong values might cause issues in the system, \nplease observe the following info to familiarise yourself with the formatting\n\n";
+		file << "1) To allow/disallow the Scrolling effect write a 1/0 next to \"Character scrolling effect\".\n\n";
+		file << "2) To change Power setting replace the value with one of the following numbers:\n - Shutdown = 1\n - Restart = 2\n - Logout = 3\n - Do Nothing = 0\n\n";
+		file << "3) To change Colour settings replace the value with one of the following numbers:\n - Light Red = 1\n - Light Purple = 2\n - Light Yellow = 3\n - Light Green = 4\n - Bright White = 5\n\n";
+		file << "General advice, please do not remove the \"#\" it will cause issues in the program because it is the divider between the stored variables and documentation.\n\nPlease only enter whole numbers as values to be loaded to avoid issues.\nDon't worry about the distance between the \"=\" sign and the actual variable I just made it like that to make it look pretty lol.\n";
 		file.close();
 	}
 }
@@ -42,6 +53,7 @@ void loadConf(short &spd, short &power, short &colour) {
 	// Extra simple algorithm to seperate variable values from other characters.
 	string x = "", y = "", z = "", line = "";
 	short count = 0;
+	bool error = false;
 	fstream file;
 	file.open("config.txt", ios::in);
 	if (file.is_open()) {
@@ -69,13 +81,37 @@ void loadConf(short &spd, short &power, short &colour) {
 			}
 		}
 	end:
+
+		// Check to see if values are valid:
+		// spd check:
+		if ((stoi(x) != 1) && (stoi(x) != 0)) {
+			error = true;
+			cout << "Error: One or more values in the config file are invalid,\ncreating another one with default values\n"; _pause(3); goto err;
+		}
+
+		// power check:
+		if ((stoi(y) < 0) || (stoi(y) > 3)) {
+			error = true;
+			cout << "Error: One or more values in the config file are invalid,\ncreating another one with default values\n"; _pause(3); goto err;
+		}
+
+		// colour check:
+		if ((stoi(z) < 1) || (stoi(z) > 5)) {
+			error = true;
+			cout << "Error: One or more values in the config file are invalid,\ncreating another one with default values\n"; _pause(3); goto err;
+		}
+		
 		// Simple conversion of the filtered digit strings into integers.
 		spd = stoi(x); power = stoi(y); colour = stoi(z);
 		file.close();
 	}
 	else {
-	sav:
-		cout << "Error: Config file not found in the current directory, \ncreating a new one with default values.\n"; _pause(3);
+		cout << "Error: Config file not found in the current directory,\ncreating a new one with default values.\n"; _pause(3);
+	err:
+		file.close();
+		if (error) {
+			_remFile();
+		}
 		saveConf(spd, power, colour);
 	}
 }
