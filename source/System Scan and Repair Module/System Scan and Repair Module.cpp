2cda74b20ac3f991b4ebaca1d1c2acb6bc8127bb
ANONYMOUSEJR <64444223+ANONYMOUSEJR@users.noreply.github.com>
2023-04-11 13:08:34 +0200

    - Minor update to the TODO list.

--- source/System Scan and Repair Module/System Scan and Repair Module.cpp
+++ source/System Scan and Repair Module/System Scan and Repair Module.cpp
@@ -9,7 +9,6 @@ int main()
 // TODO:
 // - Work on the fullscreen functions.
 // 
-// ~ Create a char counter, with a menu and the ability to allow the user to choose which chars not to include, such as spaces. ALSO, make it so that the program first scrolls out the text (as if scanning it) before displaying how many characters there are in total...
 // ~ I think that I can make like a puzzle game using the file output stuff, that could be a fun project!
 {
     // To make the program full screen.
